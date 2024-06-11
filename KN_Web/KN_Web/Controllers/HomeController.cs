04705317755b3a4e93bf47a0739479eb548afbff
Richie023 <85466516+Richie023@users.noreply.github.com>
2024-06-11 13:11:09 -0600

    Semana 2-3

---
+++ KN_Web/KN_Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KN_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Nos reedirecciona  al inicio de sesion
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+      
+    }
+}
